f6de989578a252bcca37c4c5b9da7791abb769e5
Simon Depelchin <dep.simon@gmail.com>
2022-12-09 09:46:13 +0100

    feat: create remix.init script to copy env file

---
+++ remix.init/index.js
@@ -0,0 +1,63 @@
+const { execSync } = require("child_process");
+const fs = require("fs/promises");
+const path = require("path");
+
+const semver = require("semver");
+
+const getPackageManagerCommand = (packageManager) =>
+  // Inspired by https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L38-L103
+  ({
+    npm: () => ({
+      exec: "npx",
+      lockfile: "package-lock.json",
+      run: (script, args) => `npm run ${script} ${args ? `-- ${args}` : ""}`,
+    }),
+    pnpm: () => {
+      const pnpmVersion = getPackageManagerVersion("pnpm");
+      const includeDoubleDashBeforeArgs = semver.lt(pnpmVersion, "7.0.0");
+      const useExec = semver.gte(pnpmVersion, "6.13.0");
+
+      return {
+        exec: useExec ? "pnpm exec" : "pnpx",
+        lockfile: "pnpm-lock.yaml",
+        run: (script, args) =>
+          includeDoubleDashBeforeArgs
+            ? `pnpm run ${script} ${args ? `-- ${args}` : ""}`
+            : `pnpm run ${script} ${args || ""}`,
+      };
+    },
+    yarn: () => ({
+      exec: "yarn",
+      lockfile: "yarn.lock",
+      run: (script, args) => `yarn ${script} ${args || ""}`,
+    }),
+  }[packageManager]());
+
+const getPackageManagerVersion = (packageManager) =>
+// Copied over from https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L105-L114
+execSync(`${packageManager} --version`).toString("utf-8").trim();
+
+const main = async ({ packageManager, rootDirectory }) => {
+    const pm = getPackageManagerCommand(packageManager);
+
+    const EXAMPLE_ENV_PATH = path.join(rootDirectory, '.env')
+    const ENV_PATH = path.join(rootDirectory, '.env.example')
+
+    const [exampleEnv] = await Promise.all([
+        fs.readFile(EXAMPLE_ENV_PATH, 'utf-8')
+    ])
+
+   const fileOperationPromises = [
+    fs.writeFile(ENV_PATH, exampleEnv)
+   ]
+
+   await Promise.all(fileOperationPromises)
+
+   console.log(`
+You're all set! Start building something great 🤘
+  ${pm.run("dev")}
+    `.trim()
+  );
+}
+
+module.exports = main
